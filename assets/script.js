75042c9e62cf0d0ba897305d1747b6ba8fea6ab1
Travis Fowlston <travisfowlston@ymail.com>
2023-10-21 22:48:38 -0700

    added and adjusted time blocks in index.html and added past, present, future class to each time block

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 var dateTracker = $('#currentDay')
-$(function () {
+var currentHour = dayjs().hour();
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -10,22 +10,36 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
+ 
+  /* Applies the past, present, or future class to each time block by comparing the id to the current hour. */
+  var timeBlock = function() {
+    
+    for (let index = 9; index < 18; index++) {
+      var timeSlot = $('#hour-' + index);
+      
+      if (currentHour < index) {
+        timeSlot.addClass("future");
+      } else if (currentHour > index) {
+        timeSlot.addClass("past");
+      } else {
+        timeSlot.addClass("present");
+      }
+    }
+  }
+  
+  timeBlock();
+  
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
-});
+  
+
 /* References Day.js to keep current date and time displayed */
 var updateDate = function() {
   var now = dayjs().format('MMMM DD, YYYY [at] h:mm:ss A')
-  dateTracker.text(now)
+  dateTracker.text("It is currently: " + now)
 }
-updateDate()
-setInterval(updateDate, 1000)
+
+updateDate();
+setInterval(updateDate, 1000);
